5740f725ea853012df201cf3087c8d6335c44a7f
Orlando Murcio <ato.murcio@gmail.com>
2023-06-05 14:03:14 -0600

    Set up Automapper

---
+++ leaveManagement.Web/Models/Configurations/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutoMapper;
+using leaveManagement.Web.Data;
+
+namespace leaveManagement.Web.Models.Configurations
+{
+	public class MapperConfig : Profile
+	{
+		public MapperConfig()
+		{
+			CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+		}
+	}
+}
+
