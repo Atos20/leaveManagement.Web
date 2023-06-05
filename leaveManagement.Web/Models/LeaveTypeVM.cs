e1cd5e7440ca7a43e6885bba92bbd2a7fba1e149
Orlando Murcio <ato.murcio@gmail.com>
2023-06-05 14:47:55 -0600

    Add required annotations to LeaveTypeVM

--- leaveManagement.Web/Models/LeaveTypeVM.cs
+++ leaveManagement.Web/Models/LeaveTypeVM.cs
@@ -6,8 +6,13 @@ namespace leaveManagement.Web.Models;
 public class LeaveTypeVM
 {
     public int Id { get; set; }
+
+    [Display(Name="Leave Type Name")]
+    [Required]
     public string Name { get; set; }
+
     [Display(Name="Default Number of Days")]
+    [Range(1,25,ErrorMessage ="Please Enter a Valid Number between 1 - 25")]
     public int DefaultDays { get; set; }
 
 }
