7d7c0aad783252fde81f75ba4c50e0b79e92454c
Orlando Murcio <ato.murcio@gmail.com>
2023-06-05 13:54:31 -0600

    Create Leave type model

---
+++ leaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace leaveManagement.Web.Models;
+
+public class LeaveTypeVM
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    [Display(Name="Default Number of Days")]
+    public int DefaultDays { get; set; }
+
+}
+
+
