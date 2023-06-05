47355422aa23c7fbb09097895f706ffd79b1e822
Orlando Murcio <ato.murcio@gmail.com>
2023-06-04 23:49:20 -0600

    Add new data model New LeaveAllocation, EMployee and LeaveType models are added to the DB

---
+++ leaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace leaveManagement.Web.Data
+{
+	public class LeaveAllocation
+	{
+		public int Id { get; set; }
+		public int NumberOfDays { get; set; }
+
+		//allocations have a reference for leave type
+		[ForeignKey("LeaveTypeId")]
+		public LeaveType LeaveType { get; set; }
+		public int LeaveTypeId { get; set; }
+
+		//Allocate leave type to employee
+		public string EmployeeId { get; set; }
+
+		public DateTime DateCreated { get; set; }
+		public DateTime DateModified { get; set; }
+	}
+}
+
